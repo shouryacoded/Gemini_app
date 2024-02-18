013c84a45ced2d9194dc4bd868f0c2aaaab58a1a
Shourya Parashar <shourya.online23@gmail.com>
2024-02-19 04:29:47 +0530

    Changes

--- pdfapp.py
+++ pdfapp.py
@@ -2,37 +2,43 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+# from langchain.vectorstores import FAISS # | Depreciated 
+from langchain_community.vectorstores import faiss
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
 
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
-            text+=page.extract_text()
-    return text
+            text+= page.extract_text()
+    return  text
+
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
+
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    # vector_store = faiss.from_texts(text_chunks, embedding=embeddings) # | Depreciated
+    vector_store = faiss.FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
-    
+
+
 def get_conversational_chain():
 
     prompt_template = """
@@ -45,17 +51,20 @@ def get_conversational_chain():
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
+                             temperature=0.8)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
     return chain
 
+
+
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    # new_db = faiss.load_local("faiss_index", embeddings)
+    new_db = faiss.FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
@@ -72,13 +81,8 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat with PDFs")
-    st.header("Chat with PDFs using Gemini💁")
-
-    user_question = st.text_input("Ask a Question from the PDF Files")
-
-    if user_question:
-        user_input(user_question)
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
     with st.sidebar:
         st.title("Menu:")
@@ -89,6 +93,14 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
                 st.success("Done")
+
+    if pdf_docs:
+        user_question = st.text_input("Ask a Question from the PDF Files")
+
+        if user_question:
+            user_input(user_question)
+    else:
+        st.write("Please upload the PDFs first")
 
 
 
